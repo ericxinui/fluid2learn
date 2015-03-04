782368b0c4988713edf60ff737360a52e491c05b
Lucas Schanner <lsschanner@gmail.com>
2015-03-04 18:31:16 -0300

    Melhora indentação

--- src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
+++ src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
@@ -7,40 +7,37 @@ import pt.c01interfaces.s01knowledge.s01base.inter.IEnquirer;
 import pt.c01interfaces.s01knowledge.s01base.inter.IObjetoConhecimento;
 import pt.c01interfaces.s01knowledge.s01base.inter.IResponder;
 
-public class Enquirer implements IEnquirer
-{
+public class Enquirer implements IEnquirer {
     IObjetoConhecimento obj;
-	
-	public Enquirer()
-	{
 
-	}
-	
-	
-	@Override
-	public void connect(IResponder responder)
-	{
+    public Enquirer() {
+
+    }
+
+
+    @Override
+    public void connect(IResponder responder) {
         IBaseConhecimento bc = new BaseConhecimento();
 
         obj = bc.recuperaObjeto("tiranossauro");
 
-		IDeclaracao decl = obj.primeira();
-		
-		while (decl != null && responder.ask(decl.getPropriedade()).equalsIgnoreCase(decl.getValor()))
-			decl = obj.proxima();
-		
-		boolean acertei = false;
-		
-		if (decl == null)
-			acertei = responder.finalAnswer("tiranossauro");
-		else
-			acertei = responder.finalAnswer("nao conheco");
-		
-		if (acertei)
-			System.out.println("Oba! Acertei!");
-		else
-			System.out.println("fuem! fuem! fuem!");
+        IDeclaracao decl = obj.primeira();
 
-	}
+        while (decl != null && responder.ask(decl.getPropriedade()).equalsIgnoreCase(decl.getValor()))
+            decl = obj.proxima();
+
+        boolean acertei = false;
+
+        if (decl == null)
+            acertei = responder.finalAnswer("tiranossauro");
+        else
+            acertei = responder.finalAnswer("nao conheco");
+
+        if (acertei)
+            System.out.println("Oba! Acertei!");
+        else
+            System.out.println("fuem! fuem! fuem!");
+
+    }
 
 }
