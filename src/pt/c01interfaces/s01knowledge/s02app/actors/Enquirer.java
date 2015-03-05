2869a843f9aaf07dd8c7a5a7da6cd5d183913c15
Lucas Schanner <lsschanner@gmail.com>
2015-03-05 08:14:49 -0300

    Resolve lab

--- src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
+++ src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
@@ -7,11 +7,26 @@ import pt.c01interfaces.s01knowledge.s01base.inter.IEnquirer;
 import pt.c01interfaces.s01knowledge.s01base.inter.IObjetoConhecimento;
 import pt.c01interfaces.s01knowledge.s01base.inter.IResponder;
 
+import java.util.HashMap;
+
 public class Enquirer implements IEnquirer {
     IObjetoConhecimento obj;
+    HashMap<String,String> memo;
 
     public Enquirer() {
+        memo = new HashMap<String, String>();
+    }
 
+    public String memoAsk (String pergunta,IResponder responder) {
+
+        if(memo.containsKey(pergunta)){
+            return memo.get(pergunta) ;
+        }
+        else{
+            String res = responder.ask(pergunta);
+            memo.put(pergunta,res);
+            return res;
+        }
     }
 
 
@@ -19,17 +34,31 @@ public class Enquirer implements IEnquirer {
     public void connect(IResponder responder) {
         IBaseConhecimento bc = new BaseConhecimento();
 
-        obj = bc.recuperaObjeto("tiranossauro");
+        String animais[] = {"tiranossauro", "aranha", "camarao", "humano", "pikachu"};
 
-        IDeclaracao decl = obj.primeira();
+        boolean ja_sei = false;
+        int i;
+        for (i = 0; i < animais.length && !ja_sei; i++) {
+            obj = bc.recuperaObjeto(animais[i]);
+            IDeclaracao decl = obj.primeira();
 
-        while (decl != null && responder.ask(decl.getPropriedade()).equalsIgnoreCase(decl.getValor()))
-            decl = obj.proxima();
+            ja_sei = true;
+            while(decl != null){
+                if(!memoAsk(decl.getPropriedade(),responder).equalsIgnoreCase(decl.getValor())){
+                    ja_sei = false;
+                    break;
+                }
+                decl = obj.proxima();
 
-        boolean acertei = false;
+            }
 
-        if (decl == null)
-            acertei = responder.finalAnswer("tiranossauro");
+
+        }
+
+
+        boolean acertei;
+        if (ja_sei)
+            acertei = responder.finalAnswer(animais[i-1]);
         else
             acertei = responder.finalAnswer("nao conheco");
 
