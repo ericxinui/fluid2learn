4b0ea7c65203f92842d7385edccc66f76260586e
Eric Inui <eric.inui@gmail.com>
2015-03-09 17:42:07 -0300

    commit test

--- src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
+++ src/pt/c01interfaces/s01knowledge/s02app/actors/Enquirer.java
@@ -1,5 +1,7 @@
 package pt.c01interfaces.s01knowledge.s02app.actors;
 
+import java.util.HashMap;
+
 import pt.c01interfaces.s01knowledge.s01base.impl.BaseConhecimento;
 import pt.c01interfaces.s01knowledge.s01base.inter.IBaseConhecimento;
 import pt.c01interfaces.s01knowledge.s01base.inter.IDeclaracao;
@@ -7,104 +9,56 @@ import pt.c01interfaces.s01knowledge.s01base.inter.IEnquirer;
 import pt.c01interfaces.s01knowledge.s01base.inter.IObjetoConhecimento;
 import pt.c01interfaces.s01knowledge.s01base.inter.IResponder;
 
-import java.util.HashMap;
 
-public class Enquirer implements IEnquirer {
-    IObjetoConhecimento obj;
-<<<<<<< HEAD
-    HashMap<String,String> memo;
+public class Enquirer implements IEnquirer{
 
-    public Enquirer() {
-        memo = new HashMap<String, String>();
-    }
-
-    public String memoAsk (String pergunta,IResponder responder) {
-
-        if(memo.containsKey(pergunta)){
-            return memo.get(pergunta) ;
-        }
-        else{
-            String res = responder.ask(pergunta);
-            memo.put(pergunta,res);
-            return res;
-        }
-    }
-
-
-    @Override
-    public void connect(IResponder responder) {
-        IBaseConhecimento bc = new BaseConhecimento();
-
-        String animais[] = {"tiranossauro", "aranha", "camarao", "humano", "pikachu"};
-
-        boolean ja_sei = false;
-        int i;
-        for (i = 0; i < animais.length && !ja_sei; i++) {
-            obj = bc.recuperaObjeto(animais[i]);
-            IDeclaracao decl = obj.primeira();
-
-            ja_sei = true;
-            while(decl != null){
-                if(!memoAsk(decl.getPropriedade(),responder).equalsIgnoreCase(decl.getValor())){
-                    ja_sei = false;
-                    break;
-                }
-                decl = obj.proxima();
-
-            }
-
-
-        }
-
-
-        boolean acertei;
-        if (ja_sei)
-            acertei = responder.finalAnswer(animais[i-1]);
-        else
-            acertei = responder.finalAnswer("nao conheco");
-
-        if (acertei)
-            System.out.println("Oba! Acertei!");
-        else
-            System.out.println("fuem! fuem! fuem!");
-
-    }
-=======
-	
-	public Enquirer()
-	{
-	}
-	
+	IObjetoConhecimento obj;
+	public HashMap<String, String> quests = new HashMap<String, String>();
 	
 	@Override
-	public void connect(IResponder responder)
-	{
-        IBaseConhecimento bc = new BaseConhecimento();
-		
-		obj = bc.recuperaObjeto("tiranossauro");
+	public void connect(IResponder responder) {
+		// TODO Auto-generated method stub
+		IBaseConhecimento t = new BaseConhecimento();
+		String[] v = new String[7];
+		v = t.listaNomes();
 
-		IDeclaracao decl = obj.primeira();
-		
-        boolean animalEsperado = true;
-		while (decl != null && animalEsperado) {
-			String pergunta = decl.getPropriedade();
-			String respostaEsperada = decl.getValor();
+		for (int i = 0; i < v.length; i++) {			
+			obj = t.recuperaObjeto(v[i]);
 			
-			String resposta = responder.ask(pergunta);
-			if (resposta.equalsIgnoreCase(respostaEsperada))
-				decl = obj.proxima();
-			else
-				animalEsperado = false;
+			IDeclaracao decl = obj.primeira();
+			
+	        boolean animalEsperado = true;
+			while (decl != null && animalEsperado) {
+				String pergunta = decl.getPropriedade();
+				String respostaEsperada = decl.getValor();
+				String resposta;				
+				
+				if (quests.containsKey(pergunta))
+					resposta = quests.get(pergunta);					
+				
+				else {
+					resposta = responder.ask(pergunta);
+					quests.put(pergunta, resposta);					
+				}
+																				
+				if (resposta.equalsIgnoreCase(respostaEsperada)) 
+					decl = obj.proxima();
+					
+				else
+					animalEsperado = false;
+			}
+			
+			boolean acertei = false;
+			if(animalEsperado)
+				acertei = responder.finalAnswer(v[i]);
+			
+			if (acertei){
+				System.out.println("Oba! Acertei!");
+				return;
+			} else
+				System.out.println("fuem! fuem! fuem!");
+			
 		}
-		
-		boolean acertei = responder.finalAnswer("tiranossauro");
-		
-		if (acertei)
-			System.out.println("Oba! Acertei!");
-		else
-			System.out.println("fuem! fuem! fuem!");
-
-	}
->>>>>>> origin/master
+	}		
 
 }
