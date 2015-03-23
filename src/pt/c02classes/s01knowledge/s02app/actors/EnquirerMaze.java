080e615bff92c98c1ff869cef311a924a7410158
Eric Inui <eric.inui@gmail.com>
2015-03-23 15:00:55 -0300

    atualizado com os diagramas UML

--- src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
+++ src/pt/c02classes/s01knowledge/s02app/actors/EnquirerMaze.java
@@ -6,43 +6,153 @@ import pt.c02classes.s01knowledge.s01base.inter.IResponder;
 
 public class EnquirerMaze implements IEnquirer {
 
-	private IResponder responder;
+	IResponder responder;
 	
 	public void connect(IResponder responder) {
 		this.responder = responder;
 	}
 	
-	//responder.move--->string
 	public boolean discover() {
 		
-		String norte = responder.ask("norte");
-		String sul = responder.ask("sul");
-		String leste = responder.ask("leste");
-		String oeste = responder.ask("oeste");
-		String aqui = responder.ask("aqui");
+		String N = responder.ask("norte");/*Strings Atualizadas com as posicoes em todas as direcoes*/
+		String S = responder.ask("sul");
+		String L = responder.ask("leste");
+		String O = responder.ask("oeste");
+		String A = responder.ask("aqui");
+		int cord=1;/*Variavel coordenada, utilizada para indicar o sentido de deslocamento, NORTE = 1 , Sul =2 ,Leste=3 , Oeste=4 */
+		
+		/*Caso a entrada seja Aberta em mais de um bloco de passagem ex -->(##E  ###)*/
+		while( L == "passagem"){
+			responder.move("leste");
+			L = responder.ask("leste");
+			N = responder.ask("norte");
+			S = responder.ask("sul");
+			O = responder.ask("oeste");
+			A = responder.ask("aqui");
+		}
+		
+		/*No caso em que que se atingiu a o ultimo bloco ao leste,verifica se o norte esta disponivel e o move*/
+		if(S == "mundo" && N != "parede"){
+			responder.move("norte");
+			L = responder.ask("leste");
+			N = responder.ask("norte");
+			S = responder.ask("sul");
+			O = responder.ask("oeste");
+			A = responder.ask("aqui");
+		}else if(S == "mundo" && N == "parede" ){/*Caso contrario, move para oeste*/
+			responder.move("oeste");
+			L = responder.ask("leste");
+			N = responder.ask("norte");
+			S = responder.ask("sul");
+			O = responder.ask("oeste");
+			A = responder.ask("aqui");
+			cord = 4;
+		}
+		
 
-		
-		
-		while(aqui != "saida"){
-			if(leste != "parede"){
-				responder.move("leste");
+		while(A != "saida"){
+			/*Enquanto Nao encontrar a Saida*/
+			
+			/*Sentido atual Norte*/
+			if( cord == 1){
+				if(L != "parede"){
+
+					responder.move("leste");
+					cord = 3;
+
+				}
+				else if( L == "parede" && N != "parede"){
+
+					responder.move("norte");
+					cord =1;
+
+				}
+				else if( L == "parede" && N == "parede" && O != "parede"){
+					responder.move("oeste");
+					cord =4;
+					
+				}
+				else if(L == "parede" && N == "parede" && O == "parede" && S !="parede" ){
+					responder.move("sul");
+					cord = 2;
+					
+				}
 			}
-			else if( leste == "parede" && norte != "parede"){
-				responder.move("norte");
-			}
-			else if( leste == "parede" && norte == "parede" && oeste != "parede"){
-				responder.move("oeste");
-			}
-			else if(leste == "parede" && norte == "parede" && oeste == "parede" && sul !="parede" ){
+			else if( cord == 2){			/*Sentido atual Sul*/
+
 				
-				responder.move("sul");
+				if(O != "parede"){
+					responder.move("oeste");
+					cord = 4;
+				}
+				else if( O == "parede" && S != "parede"){
+					responder.move("sul");
+					cord = 2;
+				}
+				else if( O == "parede" && S == "parede" && L != "parede"){
+					responder.move("leste");
+					cord = 3;
+					
+				}
+				else if(O == "parede" && S == "parede" && L == "parede" && N !="parede" ){
+					responder.move("norte");
+					cord = 1;
+				}
 			}
-		
-			norte = responder.ask("norte");
-			sul = responder.ask("sul");
-			leste = responder.ask("leste");
-			oeste = responder.ask("oeste");
-			aqui = responder.ask("aqui");
+			else if( cord == 3){			/*Sentido atual Leste*/
+
+				
+				if(S != "parede"){
+					responder.move("sul");
+					cord = 2;
+				}
+				else if( S == "parede" && L != "parede"){
+					responder.move("leste");
+					cord =3;
+					
+				}
+				else if( S == "parede" && L == "parede" && N != "parede"){
+					responder.move("norte");
+					cord = 1;
+					
+				}
+				else if(S == "parede" && L == "parede" &&  N== "parede" && O !="parede" ){
+					responder.move("oeste");
+					cord = 4;
+				}
+			}else if( cord == 4){			/*Sentido atual Oeste*/
+
+				
+				if(N != "parede"){
+					responder.move("norte");
+					cord = 1;
+					
+				}
+				else if( N == "parede" && O != "parede"){
+					responder.move("oeste");
+					cord = 4;
+				}
+				else if( N == "parede" && O == "parede" && S != "parede"){
+					responder.move("sul");
+					cord = 2;
+					
+				}
+				else if(N == "parede" && L == "parede" && S == "parede" && L !="parede" ){
+					responder.move("leste");
+					cord = 3;
+					
+				}
+			
+			}
+			
+			
+			/*Atualiza , a cada laco , o que existe em todas as direcoes*/
+			N = responder.ask("norte");
+			S = responder.ask("sul");
+			L = responder.ask("leste");
+			O = responder.ask("oeste");
+			A = responder.ask("aqui");;
+
 		}
 		
 		if (responder.finalAnswer("cheguei"))
